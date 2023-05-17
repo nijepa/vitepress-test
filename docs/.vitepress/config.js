8d3de2e37807af28c2ba20d6ebc03a821177dcb5
Nikola Pavicevic <nijepa@gmail.com>
2023-05-18 00:38:34 +0200

    take three

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "vit",
   description: "vit",
-  base: '/vitepress-test',
+  base: '/vitepress-test/docs',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
